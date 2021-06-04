00b3f4d39ec5f79273ba093cc590675138fba652
IMRANDIL <aliimranadil2@gmail.com>
2021-06-04 19:12:21 +0530

    yh...

--- app.js
+++ app.js
@@ -8,23 +8,86 @@ const heading3 = document.querySelector('.three');
 
 // adding event listener...
 
+// using async await...
 
-btn.addEventListener('click', () =>{
-addColor(1000,heading1,'red').then(()=>addColor(2000,heading2,'green')).then(() =>addColor(1000,heading3,'blue')).catch((err) =>console.log(err))
+
+
+btn.addEventListener('click', async () => {
+
+    try {
+        await addColor(1000, heading1, 'firebrick');
+        await addColor(2000, heading2, 'green');
+        await addColor(1000, heading3, 'blue');
+    } catch (error) {
+        console.log(error);
+    }
 })
 
 
-function addColor(t,element,color){
-    return new Promise((resolve, reject) =>{
-        if(element){
+function addColor(t, element, color) {
+    return new Promise((resolve, reject) => {
+        if (element) {
             setTimeout(() => {
                 element.style.color = color;
                 resolve();
-            },t);
-            
+            }, t);
+
         }
-        else{
+        else {
             reject(new Error(`Oh no Oh no no no ....!`));
         }
     })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// btn.addEventListener('click', () =>{
+// addColor(1000,heading1,'red')
+// .then(()=>addColor(2000,heading2,'green'))
+// .then(() =>addColor(1000,heading3,'blue'))
+// .catch((err) =>console.log(err))
+// })
+
+
+// function addColor(t,element,color){
+//     return new Promise((resolve, reject) =>{
+//         if(element){
+//             setTimeout(() => {
+//                 element.style.color = color;
+//                 resolve();
+//             },t);
+
+//         }
+//         else{
+//             reject(new Error(`Oh no Oh no no no ....!`));
+//         }
+//     })
+// }
